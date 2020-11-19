0f695b22c6fc482c46bab5cf0e7b18437d6f45dd
Muhammad Zahir Yusof <zahiryusof92@gmail.com>
2020-11-19 16:19:12 +0800

    Added API Facility, Person In Charge & Houshe Scheme Details

--- app/models/Commercial.php
+++ app/models/Commercial.php
@@ -2,4 +2,12 @@
 
 class Commercial extends Eloquent {
     protected $table = 'commercial_block';
+    
+    public function mfUnit() {
+        return $this->belongsTo('UnitOption', 'maintenance_fee_option');
+    }
+    
+    public function sfUnit() {
+        return $this->belongsTo('UnitOption', 'sinking_fund_option');
+    }
 }
