84f9bd042052327527fc4203755935a3d6f7638c
Muhammad Zahir Yusof <zahiryusof92@gmail.com>
2021-01-22 17:47:26 +0800

    Update filters.php

--- app/filters.php
+++ app/filters.php
@@ -12,7 +12,15 @@
  */
 
 App::before(function($request) {
-    //
+    if ($_SERVER['REQUEST_METHOD'] === 'OPTIONS') {
+
+        header('Access-Control-Allow-Origin', 'https://test.odesi.tech/');
+        header('Allow', 'GET, POST, OPTIONS');
+        header('Access-Control-Allow-Headers', 'Origin, Content-Type, Accept, Authorization, X-Request-With');
+        header('Access-Control-Allow-Credentials', 'true');
+
+        exit;
+    }
 });
 
 
