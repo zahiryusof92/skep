002b218fe130f2b9ea4cbf964bd95345eb295f35
chinyeep123 <45221692+chinyeep123@users.noreply.github.com>
2021-09-07 09:39:25 +0800

    Patrick (#95)
    * 22032021 patrick : commit new design
    
    * 22032021 patrick : update images
    
    * 22032021 patrick last commit css
    
    * Update login.blade.php
    
    * 22032021 patrick : update images naming
    
    * 22032021 patrick : fixed home page image not compatible
    
    * Update footer
    
    * 23032021 patrick : update navigation css, change navigation home icon, change header text css, get dashboard card total records
    
    * Update routes.php
    
    * 23032021 patrick : update navigation
    
    * Update navigation.blade.php
    
    * Update Company.php
    
    * 01042021 patrick : update layout css, export function, comment eai function, add new developer section in cob management list
    
    * 05042021 patrick : remove logo & copyright
    
    * 05042021 patrick : remove clear text in upload features
    
    * 05042021 patrick : remove bottom logo n copy right in login
    
    * 08042021 patrick : new design enchancement, bug fixed on reporting, new section in cob management list, bugs fixed in print reporting
    
    * Special customization for MBS & MPS
    
    * Fix wrong short name
    
    * 09042021 patrick : add new api for mobile dashboard
    
    * 17052021 patrick : add in enforcement payment gateway
    
    * 18052021 patrick : remove paydibs payment
    
    * 21052021 patrick : fixed summon payment return redirect url
    
    * 04062021 patrick : fixed datepicker arrow not show, mf and sf add more button, racial and foreigner percentage, finance summary and sf report calculation not correct, report strata profile, finance list add date filter, ajk start year and end year
    
    * 08062021 patrick : fixed admin cannot see the summon paid list
    
    * 08062021 patrick : fixed extra record not deleted, fixed language value
    
    * 10062021 patrick : remove duplicate navigation
    
    * 10062021 patrick : change revenue payment to sandbox
    
    * 11062021 patrick : fixed callback route from payment, date picker style, fixed navigation not show, strata add 1 more field
    
    * 14062021 patrick : filter on strata profile, design on strata tab for ecob, point never deduct, revenue callback error, list to show payment method
    
    * 14062021 patrick : fixed calculation bugs payment
    
    * 14062021 patrick : commit changes
    
    * 14062021 patrick : fixed other tabs error
    
    * 15062021 patrick : fixed finance file, strata profile filter date
    
    * 15062021 patrick : remove unuse file in config folder
    
    * 25062021 patrick : fixed bugs
    
    * 06072021 : hide transaction navigation
    
    * 06072021 patrick : update jmb expiration
    
    * 03082021 patrick : add new cob sync function with eagm
    
    * 12082021 patrick : add primary owner data checking
    
    * 13082021 patrick : recheck management fillable
    
    * patrick : commit management model
    
    * 13082021 patrick : add http host checking
    
    * 26082021 patrick : update cob file for presale role checking
    
    * patrick : commit role model
    
    * 30082021 patrick : add finance support in cob file, add print button in finance support
    
    * 01092021 patrick add checking in print finance support
    
    * 01092021 patrick : add support model
    
    * 02092021 patrick : add checking for finance support
    
    * 02092021 patrick : update
    
    * 02092021 patrick : update
    
    * 02092021 patrick : update finance support date
    
    * 02092021 patrick : fix file no not found issue
    
    * 06092021 patrick : add reset mbs password function
    
    * 06092021 patrick update file
    
    * 07092021 patrick : mbs reset password function update
    
    Co-authored-by: Muhammad Zahir Yusof <zahiryusof92@gmail.com>

--- app/controllers/MBSController.php
+++ app/controllers/MBSController.php
@@ -1,6 +1,7 @@
 <?php
 
 use Carbon\Carbon;
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Hash;
 use Maatwebsite\Excel\Facades\Excel;
@@ -18,32 +19,49 @@ class MBSController extends BaseController {
     }
 
     public function resetJMB() {
-        $cob = Company::where('short_name', 'MBS')->first();
-        $role = Role::where('name', 'JMB')->first();
-        $users = User::where('role', $role->id)
-                    ->where('company_id', $cob->id)
-                    ->where('is_deleted',0)
-                    ->get();
-                    
-        $data = [];
-        if(count($users) > 0) {
-            foreach($users as $user) {
-                $new_password = self::generateRandomString();
-                if(empty($user->username) == false) {
-                    $user->update([
-                        'password' => Hash::make($new_password)
-                    ]);
-                }
+        if(Auth::check() && Auth::user()->getAdmin()) {
+            $cob = Company::where('short_name', 'MBS')->first();
+            if(!empty($cob)) {
+                $role = Role::where('name', 'JMB')->first();
+                $users = User::where('role', $role->id)
+                            ->where('company_id', $cob->id)
+                            ->where('is_deleted',0)
+                            ->get();
+                            
+                $data = [];
+                if(count($users) > 0) {
+                    foreach($users as $user) {
+                        $new_password = self::generateRandomString();
+                        $user->update([
+                            'password' => Hash::make($new_password)
+                        ]);
+                        
+                        $arr = [
+                            'name' => $user->full_name,
+                            'email' => $user->email,
+                            'strata' => !empty($user->file_id)? Strata::where('file_id', $user->file_id)->first()->name: '-',
+                            'username' => $user->username,
+                            'password' => $new_password,
+                            'phone_no' => !empty($user->phone_no)? $user->phone_no : '-',
 
-                array_push($data, ['username' => $user->username, 'password' => $new_password]);
+                        ];
+                        array_push($data, $arr);
+                    }
+                }
+                
+                return Excel::create('password_list-'. date('YmdHis'), function($excel) use ($data) {
+                    $excel->sheet('mySheet', function($sheet) use ($data)
+                    {
+                        $sheet->fromArray($data);
+                    });
+                })->download();
+
             }
+        } else {
+            return [
+                'error' => true,
+                'message' => 'fail'
+            ];
         }
-        
-        return Excel::create('password_list', function($excel) use ($data) {
-            $excel->sheet('mySheet', function($sheet) use ($data)
-            {
-                $sheet->fromArray($data);
-            });
-        })->download();
     }
 }
