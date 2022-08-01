470cad5a8329aa6d9658e41a70caa29496c61463
Muhammad Zahir Yusof <zahiryusof92@gmail.com>
2022-08-01 10:16:17 +0800

    Added email validate

--- app/helper/Helper.php
+++ app/helper/Helper.php
@@ -12,21 +12,22 @@ use Illuminate\Support\Facades\View;
 
 class Helper
 {
-    public static function isAllow($file_id = 0, $company_id = 0, $has_access = 0) {
+    public static function isAllow($file_id = 0, $company_id = 0, $has_access = 0)
+    {
         $disallow = false;
         if (!Auth::user()->getAdmin()) {
             if (!empty(Auth::user()->file_id)) {
-                if(($company_id > 0 && $company_id != Auth::user()->company_id) || ($file_id > 0 && $file_id != Auth::user()->file_id) || $has_access) {
+                if (($company_id > 0 && $company_id != Auth::user()->company_id) || ($file_id > 0 && $file_id != Auth::user()->file_id) || $has_access) {
                     App::abort(404);
                 }
             } else {
-                if(($company_id > 0 && $company_id != Auth::user()->company_id) || $has_access) {
+                if (($company_id > 0 && $company_id != Auth::user()->company_id) || $has_access) {
                     App::abort(404);
                 }
             }
         } else {
             if (!empty(Session::get('admin_cob'))) {
-                if(($company_id > 0 && $company_id != Session::get('admin_cob'))) {
+                if (($company_id > 0 && $company_id != Session::get('admin_cob'))) {
                     App::abort(404);
                 }
             }
@@ -37,7 +38,7 @@ class Helper
 
     public static function encode(...$args)
     {
-        if(count($args) == 1) {
+        if (count($args) == 1) {
             return (new Hashids('', 16))->encode(...$args);
         }
         return (new Hashids($args[0], 16))->encode($args[1]);
@@ -49,7 +50,7 @@ class Helper
             return $enc;
         }
         $id = (new Hashids($module, 16))->decode($enc);
-        if(empty($id)) {
+        if (empty($id)) {
             App::abort(404);
         }
         return $id[0];
@@ -58,42 +59,51 @@ class Helper
     /**
      * Difference with 2 array and find out the differences
      */
-    public static function check_diff_multi($array1, $array2){
+    public static function check_diff_multi($array1, $array2)
+    {
         $result = array();
-        foreach($array1 as $key => $val) {
-            if(is_array($val) && isset($array2[$key])) {
+        foreach ($array1 as $key => $val) {
+            if (is_array($val) && isset($array2[$key])) {
                 $tmp = self::check_diff_multi($val, $array2[$key]);
-                if($tmp) {
+                if ($tmp) {
                     $result[$key] = $tmp;
                 }
-            }
-            elseif(!isset($array2[$key])) {
+            } elseif (!isset($array2[$key])) {
                 $result[$key] = null;
-            }
-            elseif($val !== $array2[$key]) {
-                if(!in_array($key, ['id', 'created_at', 'updated_at'])) {
+            } elseif ($val !== $array2[$key]) {
+                if (!in_array($key, ['id', 'created_at', 'updated_at'])) {
                     $result[$key] = $array2[$key];
                 }
             }
-    
-            if(isset($array2[$key])) {
+
+            if (isset($array2[$key])) {
                 unset($array2[$key]);
             }
         }
-    
+
         $result = array_merge($result, $array2);
-    
+
         return $result;
     }
 
     /**
      * Replace the search last character
      */
-    public static function str_replace_last( $search , $replace , $str ) {
-        if( ( $pos = strrpos( $str , $search ) ) !== false ) {
-            $search_length  = strlen( $search );
-            $str    = substr_replace( $str , $replace , $pos , $search_length );
+    public static function str_replace_last($search, $replace, $str)
+    {
+        if (($pos = strrpos($str, $search)) !== false) {
+            $search_length  = strlen($search);
+            $str    = substr_replace($str, $replace, $pos, $search_length);
         }
         return $str;
-    }  
+    }
+
+    public static function validateEmail($email)
+    {
+        if (filter_var($email, FILTER_VALIDATE_EMAIL)) {
+           return true;
+        }
+
+        return false;
+    }
 }
