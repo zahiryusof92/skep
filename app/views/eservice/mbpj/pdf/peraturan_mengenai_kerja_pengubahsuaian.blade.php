45ffa8160a1496c579ba9e81ca4025650f1400a4
Muhammad Zahir Yusof <zahiryusof92@gmail.com>
2023-09-21 01:03:28 +0800

    E perkhidmatan mbpj (#269)
    * Added payment gateway
    
    * Update EServiceController.php
    
    * payment enhancement
    
    * Update list.blade.php
    
    * Update Epay.php
    
    * fix wrong url redirection
    
    * letter enhancements
    
    * make uppercase
    
    * Update EServiceController.php
    
    * Update EServiceController.php
    
    * added log
    
    * Update EServiceController.php
    
    * clear debug log
    
    * remove json_encode
    
    * payment enhancements
    
    * allow admin & cob edit e-perkhidmatan
    
    * Update surat_kebocoran_antara_tingkat.blade.php
    
    * Update surat_kebocoran_antara_tingkat.blade.php
    
    * Update signature_tag_2.blade.php
    
    * allow edit rejected
    
    * added more letter templates
    
    * added more letter templates
    
    * Update EServicePriceController.php
    
    * Update EServicePriceController.php
    
    * enhancement
    
    * add more letters

---
+++ app/views/eservice/mbpj/pdf/peraturan_mengenai_kerja_pengubahsuaian.blade.php
@@ -0,0 +1,251 @@
+@extends('layout.english_layout.eservice')
+
+@section('content')
+    <table border="0" cellspacing="0" cellpadding="0" style="width: 100%;">
+        <tr>
+            <td style="width: 8%;">
+                &nbsp;
+            </td>
+            <td style="width: 5%;">
+                &nbsp;
+            </td>
+            <td style="width: 2%;">
+                &nbsp;
+            </td>
+            <td style="width: 23%;">
+                &nbsp;
+            </td>
+            <td style="width: 10%;">
+                &nbsp;
+            </td>
+            <td style="width: 2%;">
+                &nbsp;
+            </td>
+            <td style="width: 50%;">
+                &nbsp;
+            </td>
+        </tr>
+        <tr>
+            <td colspan="4">
+                &nbsp;
+            </td>
+            <td style="text-align: left; vertical-align: top;">
+                Ruj. Kami
+            </td>
+            <td style="text-align: center; vertical-align: top;">
+                :
+            </td>
+            <td>
+                {{ !empty($order->bill_no) ? $order->bill_no : '' }}
+            </td>
+        </tr>
+        <tr>
+            <td colspan="4">
+                &nbsp;
+            </td>
+            <td style="text-align: left; vertical-align: top;">
+                Tarikh
+            </td>
+            <td style="text-align: center; vertical-align: top;">
+                :
+            </td>
+            <td>
+                {{ !empty($order->date) ? \Helper\Helper::localizedDate($order->date) : '' }}
+            </td>
+        </tr>
+        <tr>
+            <td colspan="7">
+                &nbsp;
+            </td>
+        </tr>
+        <tr>
+            <td colspan="7">
+                &nbsp;
+            </td>
+        </tr>
+        <tr>
+            <td colspan="7">
+                <strong>
+                    {{ isset($content['owner_name']) ? $content['owner_name'] : '' }}
+                </strong>
+            </td>
+        </tr>
+        <tr>
+            <td colspan="7">
+                {{ isset($content['owner_address1']) ? $content['owner_address1'] : '' }}
+            </td>
+        </tr>
+        <tr>
+            <td colspan="7">
+                {{ isset($content['owner_address2']) ? $content['owner_address2'] : '' }}
+            </td>
+        </tr>
+        <tr>
+            <td colspan="7">
+                {{ isset($content['owner_address3']) ? $content['owner_address3'] : '' }}
+            </td>
+        </tr>
+        <tr>
+            <td colspan="7">
+                {{ isset($content['owner_postcode']) ? $content['owner_postcode'] : '' }}
+                {{ isset($content['owner_city']) ? $content['owner_city'] : '' }}
+            </td>
+        </tr>
+        <tr>
+            <td colspan="7">
+                {{ isset($content['owner_state']) ? $content['owner_state'] : '' }}
+            </td>
+        </tr>
+        <tr>
+            <td colspan="7">
+                &nbsp;
+            </td>
+        </tr>
+        <tr>
+            <td colspan="7">
+                Tuan/Puan,
+            </td>
+        </tr>
+        <tr>
+            <td colspan="7">
+                &nbsp;
+            </td>
+        </tr>
+        <tr>
+            <td colspan="7" style="text-align: justify;">
+                <strong>
+                    PERATURAN MENGENAI KERJA PENGUBAHSUAIAN DAN PEMBAIKAN
+                </strong>
+            </td>
+        </tr>
+        <tr>
+            <td colspan="7">
+                &nbsp;
+            </td>
+        </tr>
+        <tr>
+            <td colspan="7">
+                Dengan hormatnya saya merujuk kepada perkara di atas.
+            </td>
+        </tr>
+        <tr>
+            <td colspan="7">
+                &nbsp;
+            </td>
+        </tr>
+        <tr>
+            <td colspan="7" style="text-align: justify;">
+                <span style="padding-right: 1em;">2.</span>Pentadbiran ini telah menerima aduan daripada pihak pengurusan
+                mengenai kerja pengubahsuaian/ pembaikan yang dilaksanakan di petak no.
+                <strong>{{ isset($content['unit_no']) ? $content['unit_no'] : '' }}</strong>.
+            </td>
+        </tr>
+        <tr>
+            <td colspan="7">
+                &nbsp;
+            </td>
+        </tr>
+        <tr>
+            <td colspan="7" style="text-align: justify;">
+                <span style="padding-right: 1em;">3.</span>Justeru, pentadbiran ini ingin menarik perhatian pihak tuan
+                mengenai subperenggan 27 (1), (3), dan (4), Undang-undang Kecil di bawah Jadual Ketiga Peraturan-peraturan
+                Pengurusan Strata (Penyenggaraan dan Pengurusan) 2015 yang menyebut-
+            </td>
+        </tr>
+        <tr>
+            <td colspan="7">
+                &nbsp;
+            </td>
+        </tr>
+        <tr>
+            <td colspan="7" style="text-align: justify; font-style: italic; padding-left: 2em;">
+                “(1) pemilik hendaklah tidak menjalankan apa-apa kerja pengubahsuaian kepada petaknya tanpa mula-mula
+                mendapatkan kelulusan bertulis terdahulu dari pengurusan perbadanan dan, apabila perlu, dari pihak berkuasa
+                berkenaan dengan.”
+            </td>
+        </tr>
+        <tr>
+            <td colspan="7">
+                &nbsp;
+            </td>
+        </tr>
+        <tr>
+            <td colspan="7" style="text-align: justify; font-style: italic; padding-left: 2em;">
+                (3) menjadi tanggungjawab sendiri pemilik untuk menyemak dengan pihak berkuasa berkenaan dengan tentang
+                keperluan apa-apa kelulusan untuk melaksanakan kerja pengubahsuaian dan pemilik hendaklah merujuk perkara
+                itu dengan pihak berkuasa berkenaan dengan atas ikhtiar sendiri. Jika pemilik memohon untuk apa-apa
+                kelulusan dari perbadanan pengurusan untuk kebenaran melaksanakan kerja pengubahsuaian, perbadanan
+                pengurusan berhak menganggap bahawa pemilik telah mendapat kelulusan yang perlu dari pihak berkuasa
+                berkenaan dengan, apabila perlu, dan salinan kelulusan dari pihak berkuasa berkenaan dengan itu hendaklah
+                diberi kepada perbadanan pengurusan pada masa permohonan untuk kelulusan oleh perbadanan pengurusan. Jika
+                perbadanan pengurusan memberi kelulusan untuk apa-apa kerja pengubahsuaian dan ia kemudian mendapati bahawa
+                kelulusan yang diperlukan dari pihak berkuasa berkenaan dengan tidak diperolehi atau tidak diperolehi dengan
+                wajar, pemilik hendaklah dengan sendiri bertanggungjawab kepada pihak berkuasa berkenaan dengan dan
+                kelulusan yang diberi oleh perbadanan pengurusan untuk kerja pengubahsuaian itu hendaklah dianggap langsung
+                ditarik balik.
+            </td>
+        </tr>
+        <tr>
+            <td colspan="7">
+                &nbsp;
+            </td>
+        </tr>
+        <tr>
+            <td colspan="7" style="text-align: justify; font-style: italic; padding-left: 2em;">
+                (4) semua kerja pengubahsuaian dalam petak hendaklah terhad kepada sempadan petak itu dan hendaklah tidak
+                ada kerja dilaksanakan atas mana-mana harta bersama.”
+            </td>
+        </tr>
+        <tr>
+            <td colspan="7">
+                &nbsp;
+            </td>
+        </tr>
+        <tr>
+            <td colspan="7" style="text-align: justify;">
+                <span style="padding-right: 1em;">4.</span>Selaras dengan peraturan di atas, pentadbiran ini ingin
+                menegaskan bahawa, sebarang kerja pengubahsuaian/ pembinaan yang ingin dilaksanakan hendaklah dirujuk
+                terlebih dahulu dengan pihak pengurusan dan Jabatan Kawalan Bangunan untuk mendapatkan kelulusan bertulis.
+                Tiada kelulusan boleh diberikan bagi kerja pengubahsuaian/ pembinaan yang melebihi sempadan petak dan atas
+                harta bersama.
+            </td>
+        </tr>
+        <tr>
+            <td colspan="7">
+                &nbsp;
+            </td>
+        </tr>
+        <tr>
+            <td colspan="7" style="text-align: justify;">
+                <span style="padding-right: 1em;">5.</span>Sehubungan itu, pihak tuan dikehendaki mematuhi peraturan yang
+                telah ditetapkan untuk sama-sama menjaga, memelihara dan mengekalkan keharmonian dan kesejahteraan penduduk.
+                Sekiranya ingkar, maka pihak pengurusan boleh mengenakan denda tidak lebih dua ratus ringgit mengikut jumlah
+                yang ditentukan oleh mesyuarat agung. Selanjutnya, tindakan pendakwaan dan perobohan boleh diambil terhadap
+                pemilik sekiranya pemilik masih ingkar terhadap notis yang telah disampaikan.
+            </td>
+        </tr>
+        <tr>
+            <td colspan="7">
+                &nbsp;
+            </td>
+        </tr>
+        <tr>
+            <td colspan="7">
+                Kerjasama daripada pihak tuan amat dihargai.
+            </td>
+        </tr>
+        <tr>
+            <td colspan="7">
+                &nbsp;
+            </td>
+        </tr>
+        <tr>
+            <td colspan="7">
+                Sekian, terima kasih.
+            </td>
+        </tr>
+
+        @include('eservice.mbpj.component.signature_tag_4', ['sk' => true])
+
+    </table>
+@endsection
---
+++ app/views/eservice/mbpj/word/peraturan_pemeliharaan_haiwan_di_skim_strata.blade.php
@@ -0,0 +1,282 @@
+@extends('layout.english_layout.eservice_word')
+
+@section('content')
+    <table border="0" cellspacing="0" cellpadding="0" style="width: 100%;">
+        <tr>
+            <td style="width: 8%;">
+                &nbsp;
+            </td>
+            <td style="width: 5%;">
+                &nbsp;
+            </td>
+            <td style="width: 2%;">
+                &nbsp;
+            </td>
+            <td style="width: 23%;">
+                &nbsp;
+            </td>
+            <td style="width: 10%;">
+                &nbsp;
+            </td>
+            <td style="width: 2%;">
+                &nbsp;
+            </td>
+            <td style="width: 50%;">
+                &nbsp;
+            </td>
+        </tr>
+        <tr>
+            <td colspan="4">
+                &nbsp;
+            </td>
+            <td style="text-align: left; vertical-align: top;">
+                Ruj. Kami
+            </td>
+            <td style="text-align: center; vertical-align: top;">
+                :
+            </td>
+            <td>
+                {{ !empty($order->bill_no) ? $order->bill_no : '' }}
+            </td>
+        </tr>
+        <tr>
+            <td colspan="4">
+                &nbsp;
+            </td>
+            <td style="text-align: left; vertical-align: top;">
+                Tarikh
+            </td>
+            <td style="text-align: center; vertical-align: top;">
+                :
+            </td>
+            <td>
+                {{ !empty($order->date) ? \Helper\Helper::localizedDate($order->date) : '' }}
+            </td>
+        </tr>
+        <tr>
+            <td colspan="7">
+                &nbsp;
+            </td>
+        </tr>
+        <tr>
+            <td colspan="7">
+                &nbsp;
+            </td>
+        </tr>
+        <tr>
+            <td colspan="7">
+                <strong>
+                    {{ isset($content['building_name']) ? $content['building_name'] : '' }}
+                </strong>
+            </td>
+        </tr>
+        <tr>
+            <td colspan="7">
+                <strong>
+                    {{ isset($content['management_name']) ? $content['management_name'] : '' }}
+                </strong>
+            </td>
+        </tr>
+        <tr>
+            <td colspan="7">
+                {{ isset($content['management_address1']) ? $content['management_address1'] : '' }}
+            </td>
+        </tr>
+        <tr>
+            <td colspan="7">
+                {{ isset($content['management_address2']) ? $content['management_address2'] : '' }}
+            </td>
+        </tr>
+        <tr>
+            <td colspan="7">
+                {{ isset($content['management_address3']) ? $content['management_address3'] : '' }}
+            </td>
+        </tr>
+        <tr>
+            <td colspan="7">
+                {{ isset($content['management_postcode']) ? $content['management_postcode'] : '' }}
+                {{ isset($content['management_city']) ? $content['management_city'] : '' }}
+            </td>
+        </tr>
+        <tr>
+            <td colspan="7">
+                {{ isset($content['management_state']) ? $content['management_state'] : '' }}
+            </td>
+        </tr>
+        <tr>
+            <td colspan="7">
+                &nbsp;
+            </td>
+        </tr>
+        <tr>
+            <td colspan="7">
+                Tuan/Puan,
+            </td>
+        </tr>
+        <tr>
+            <td colspan="7">
+                &nbsp;
+            </td>
+        </tr>
+        <tr>
+            <td colspan="7" style="text-align: justify;">
+                <strong>
+                    ADUAN PEMELIHARAAN HAIWAN PELIHARAAN DI SKIM STRATA
+                </strong>
+            </td>
+        </tr>
+        <tr>
+            <td colspan="7">
+                &nbsp;
+            </td>
+        </tr>
+        <tr>
+            <td colspan="7">
+                Dengan hormatnya saya merujuk kepada perkara di atas.
+            </td>
+        </tr>
+        <tr>
+            <td colspan="7">
+                &nbsp;
+            </td>
+        </tr>
+        <tr>
+            <td colspan="7" style="text-align: justify;">
+                <span style="padding-right: 1em;">2.</span>Pentadbiran ini telah menerima aduan daripada pemilik mengenai
+                pemeliharaan haiwan peliharaan
+                <strong>{{ isset($content['type_of_animal']) ? strtolower($content['type_of_animal']) : '' }}</strong>
+                di petak no. <strong>{{ isset($content['affected_unit']) ? $content['affected_unit'] : '' }}</strong>
+                sehingga menyebabkan kegusaran dan kacau ganggu kepada pemilik lain.
+            </td>
+        </tr>
+        <tr>
+            <td colspan="7">
+                &nbsp;
+            </td>
+        </tr>
+        <tr>
+            <td colspan="7" style="text-align: justify;">
+                <span style="padding-right: 1em;">3.</span>Justeru, pentadbiran ini ingin menarik perhatian pihak tuan
+                mengenai bidang kuasa pihak tuan di bawah Subperenggan 14 (1) dan (2) Undang-undang Kecil Jadual ketiga,
+                Peraturan-peraturan Pengurusan Strata (Penyenggaraan dan Pengurusan) 2015 seperti berikut:
+            </td>
+        </tr>
+        <tr>
+            <td colspan="7">
+                &nbsp;
+            </td>
+        </tr>
+        <tr>
+            <td colspan="7" style="text-align: justify; font-style: italic; padding-left: 2em;">
+                “(1) dalam bangunan yang digunakan untuk tujuan kediaman atau tempat tinggal, pemilik hendaklah tidak
+                menyimpan apa-apa haiwan tertentu dalam petaknya atau atas harta bersama yang boleh menyebabkan kegusaran
+                atau kacau ganggu kepada pemilik lain atau yang bertentangan dengan mana-mana undang-undang atau peraturan
+                dan kaedah-kaedah bertulis pihak berkuasa negeri atau tempatan yang berkenaan dengan.
+            </td>
+        </tr>
+        <tr>
+            <td colspan="7">
+                &nbsp;
+            </td>
+        </tr>
+        <tr>
+            <td colspan="7" style="text-align: justify; font-style: italic; padding-left: 2em;">
+                (2) pemilik yang mengingkari subperenggan 14(1) undang-undang kecil ini, hendaklah dalam masa tiga hari
+                penerimaan suatu notis bertulis dari perbadanan pengurusan mengeluarkan haiwan tersebut dari bangunan. Jika
+                gagal berbuat demikian, perbadanan pengurusan boleh mengambil apa-apa tindakan dianggap perlu untuk
+                mengeluarkan haiwan itu dari bangunan dan –
+            </td>
+        </tr>
+        <tr>
+            <td colspan="7">
+                &nbsp;
+            </td>
+        </tr>
+        <tr>
+            <td colspan="7" style="text-align: justify; font-style: italic; padding-left: 3em;">
+                (a) semua kos terakru hendaklah dicaj dan dikenakan terhadap pemilik itu, dan<br />
+                (b) perbadanan pengurusan tidak bertanggungan terhadap apa-apa kerosakan pada harta pemilik yang secara
+                munasabah disebabkan oleh proses mengeluarkan haiwan itu.”
+            </td>
+        </tr>
+        <tr>
+            <td colspan="7">
+                &nbsp;
+            </td>
+        </tr>
+        <tr>
+            <td colspan="7" style="text-align: justify;">
+                <span style="padding-right: 1em;">4.</span>Bagi meningkatkan lagi kawalan ke atas aktiviti pemeliharaan
+                haiwan peliharaan di kawasan pemajuan pihak tuan, pihak tuan dinasihatkan supaya membuat undang-undang kecil
+                tambahan bagi mewujudkan suatu peraturan tambahan yang lebih khusus bagi mengawal selia kawalan terhadap
+                pemeliharaan binatang peliharaan serta pengenaan denda yang tidak melebihi dua ratus ringgit terhadap
+                mana-mana pemilik, penghuni atau jemputan yang melanggari mana-mana undang-undang kecil tersebut melalui
+                ketetapan khas dalam mesyuarat agung seperti yang diperuntukan di bawah Seksyen 32(3)(c) dan (i) atau
+                Seksyen 70(3) (c) & (i) yang mana berkenaan. Jumlah denda yang akan dikenakan hendaklah ditentukan oleh
+                mesyuarat agung seperti yang dikehendaki di bawah subperenggan 7(1), Jadual Ketiga Undang-undang Kecil
+                Peraturan-peraturan Pengurusan Strata (Penyenggaraan dan Pengurusan) 2015.
+            </td>
+        </tr>
+        <tr>
+            <td colspan="7">
+                &nbsp;
+            </td>
+        </tr>
+        <tr>
+            <td colspan="7" style="text-align: justify;">
+                <span style="padding-right: 1em;">5.</span>Semua belanja yang dibuat untuk mengeluarkan haiwan itu dan denda
+                yang dikenakan di bawah subperenggan 7(1) Undang-Undang Kecil tersebut hendaklah menjadi suatu hutang yang
+                terakru kepada pihak pengurusan dan atas pembayaran hendaklah didepositkan dalam akaun penyenggaraan.
+            </td>
+        </tr>
+        <tr>
+            <td colspan="7">
+                &nbsp;
+            </td>
+        </tr>
+        <tr>
+            <td colspan="7" style="text-align: justify;">
+                <span style="padding-right: 1em;">6.</span>Sehubungan dengan itu, pihak tuan dinasihatkan supaya
+                mengeluarkan notis kepada semua pemilik/ penghuni petak berhubung peraturan pemeliharaan haiwan peliharaan
+                di skim strata. Sebagai alternatif, pihak tuan boleh memohon kepada pentadbiran ini untuk mengeluarkan surat
+                kepada pemilik yang terlibat berhubung peraturan pemeliharaan haiwan di skim strata. Permohonan boleh dibuat
+                melalui sistem eCOB.
+            </td>
+        </tr>
+        <tr>
+            <td colspan="7">
+                &nbsp;
+            </td>
+        </tr>
+        <tr>
+            <td colspan="7" style="text-align: justify;">
+                <span style="padding-right: 1em;">7.</span>Pihak tuan juga dinasihatkan supaya menguatkuasakan undang-undang
+                kecil tersebut di atas agar masalah ini dapat dikawal bagi melindungi hak dan kepentingan bersama serta
+                dapat memelihara dan mengekalkan kerharmonian dan kesejahteraan penduduk.
+            </td>
+        </tr>
+        <tr>
+            <td colspan="7">
+                &nbsp;
+            </td>
+        </tr>
+        <tr>
+            <td colspan="7">
+                Kerjasama daripada pihak tuan amat dihargai.
+            </td>
+        </tr>
+        <tr>
+            <td colspan="7">
+                &nbsp;
+            </td>
+        </tr>
+        <tr>
+            <td colspan="7">
+                Sekian, terima kasih.
+            </td>
+        </tr>
+
+        @include('eservice.mbpj.component.signature_tag_3', ['sk' => true])
+
+    </table>
+@endsection
